c304ca31b1a348164df051ef3adb9c3d979bb250
Ross Light <ross@zombiezen.com>
2013-02-07 20:48:37 -0800

    Add the too many commands error for mockCommand

--- vcs/exec_test.go
+++ vcs/exec_test.go
@@ -6,11 +6,17 @@ import (
 	"io"
 )
 
-var errMockCmd = errors.New("mock command")
+var (
+	errMockCmd = errors.New("mock command")
+	errTooManyCmds = errors.New("too many commands")
+)
 
 type mockCommander []mockCommand
 
 func (mc *mockCommander) command(program string, args ...string) command {
+	if len(*mc) == 0 {
+		return &mockCommand{Bad: true}
+	}
 	var c *mockCommand
 	c, *mc = &(*mc)[0], (*mc)[1:]
 	c.Args = append([]string{program}, args...)
@@ -22,6 +28,8 @@ type mockCommand struct {
 
 	Args []string
 	Dir  string
+
+	Bad bool
 }
 
 func (mc *mockCommand) SetDir(dir string) {
@@ -29,37 +37,61 @@ func (mc *mockCommand) SetDir(dir string) {
 }
 
 func (mc *mockCommand) CombinedOutput() ([]byte, error) {
+	if mc.Bad {
+		return nil, errTooManyCmds
+	}
 	// TODO: stderr
 	return mc.Output()
 }
 
 func (mc *mockCommand) Output() ([]byte, error) {
+	if mc.Bad {
+		return nil, errTooManyCmds
+	}
 	b := make([]byte, mc.Out.Len())
 	copy(b, mc.Out.Bytes())
 	return b, nil
 }
 
 func (mc *mockCommand) Run() error {
+	if mc.Bad {
+		return errTooManyCmds
+	}
 	mc.Out.Truncate(0)
 	return nil
 }
 
 func (mc *mockCommand) Start() error {
+	if mc.Bad {
+		return errTooManyCmds
+	}
 	return errMockCmd
 }
 
 func (mc *mockCommand) StderrPipe() (io.ReadCloser, error) {
+	if mc.Bad {
+		return nil, errTooManyCmds
+	}
 	return nil, errMockCmd
 }
 
 func (mc *mockCommand) StdinPipe() (io.WriteCloser, error) {
+	if mc.Bad {
+		return nil, errTooManyCmds
+	}
 	return nil, errMockCmd
 }
 
 func (mc *mockCommand) StdoutPipe() (io.ReadCloser, error) {
+	if mc.Bad {
+		return nil, errTooManyCmds
+	}
 	return nil, errMockCmd
 }
 
 func (mc *mockCommand) Wait() error {
+	if mc.Bad {
+		return errTooManyCmds
+	}
 	return errMockCmd
 }
