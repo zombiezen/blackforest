e18507d5e9e4a2a6dfd9d5ec4729fbff518fbddd
Ross Light <ross@zombiezen.com>
2013-02-07 19:59:07 -0800

    Add tests for parsing identify output

--- vcs/hg_test.go
+++ vcs/hg_test.go
@@ -8,6 +8,8 @@ import (
 
 const desiredWC = "/wc"
 
+var magicHgRev = mercurialRev{0x0d, 0x9c, 0x2b, 0x3c, 0x7b, 0xce, 0x68, 0xef, 0x99, 0x50, 0xd2, 0x37, 0xea, 0xc5, 0xff, 0x67, 0xf1, 0x17, 0xbf, 0xf5}
+
 func newIsolatedMercurialWC(path string, c mockCommander) *mercurialWC {
 	return &mercurialWC{
 		hg:   &Mercurial{Program: "hg", commander: &c},
@@ -24,7 +26,7 @@ func TestMercurialCurrent(t *testing.T) {
 	if err != nil {
 		t.Errorf("wc.Current() error: %v", err)
 	}
-	if r := (mercurialRev{0x0d, 0x9c, 0x2b, 0x3c, 0x7b, 0xce, 0x68, 0xef, 0x99, 0x50, 0xd2, 0x37, 0xea, 0xc5, 0xff, 0x67, 0xf1, 0x17, 0xbf, 0xf5}); rev != r {
+	if r := magicHgRev; rev != r {
 		t.Errorf("wc.Current() = %v; want %v", rev.Rev(), r.Rev())
 	}
 	if d := mc[0].Dir; d != desiredWC {
@@ -32,5 +34,33 @@ func TestMercurialCurrent(t *testing.T) {
 	}
 	if args, want := mc[0].Args, ([]string{"hg", "identify", "--debug", "-i"}); !reflect.DeepEqual(args, want) {
 		t.Errorf("args = %v; want %v", args, want)
+	}
+}
+
+func TestParseIdentifyOutput(t *testing.T) {
+	tests := []struct {
+		Arg   string
+		Rev   mercurialRev
+		Error bool
+	}{
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff5", magicHgRev, false},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff5\n", magicHgRev, false},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff5+", magicHgRev, false},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff5+\n", magicHgRev, false},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff", mercurialRev{}, true},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff\n", mercurialRev{}, true},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff50", mercurialRev{}, true},
+		{"0d9c2b3c7bce68ef9950d237eac5ff67f117bff50\n", mercurialRev{}, true},
+	}
+	for _, test := range tests {
+		rev, err := parseIdentifyOutput([]byte(test.Arg))
+		if err != nil && !test.Error {
+			t.Errorf("parseIdentifyOutput(%q) error: %v", test.Arg, err)
+		} else if err == nil && test.Error {
+			t.Errorf("parseIdentifyOutput(%q) expected an error", test.Arg)
+		}
+		if rev != test.Rev {
+			t.Errorf("parseIdentifyOutput(%q) = %v; want %v", test.Arg, rev.Rev(), test.Rev.Rev())
+		}
 	}
 }
