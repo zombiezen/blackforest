c419dcc64d3b2c19efc01f3a095c1568159c6a76
Ross Light <ross@zombiezen.com>
2013-03-04 12:07:27 -0800

    Add GPUSH/GPULL aliases

--- misc/ross-aliases.zsh
+++ misc/ross-aliases.zsh
@@ -6,3 +6,6 @@ gcd() { cd "$(glados path "$1")"; return $? }
 compdef '_values "glados projects" $(glados list 2>/dev/null)' gcd
 
 ginfo() { glados show "$1" | sed -n 's/^'"$2"':\s*//p' }
+
+alias GPUSH='hg push -R "$GLADOS_PATH"'
+alias GPULL='hg pull -R "$GLADOS_PATH" -u'
