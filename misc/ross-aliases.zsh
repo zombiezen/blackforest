55abb9b6bc2d2720f9358f9e1443e0da7941d0b3
Ross Light <ross@zombiezen.com>
2013-03-04 11:47:40 -0800

    Fix exit in ginfo alias

--- misc/ross-aliases.zsh
+++ misc/ross-aliases.zsh
@@ -5,4 +5,4 @@ alias G=glados
 gcd() { cd "$(glados path "$1")"; return $? }
 compdef '_values "glados projects" $(glados list 2>/dev/null)' gcd
 
-ginfo { ginfo() { glados show "$1" | sed -n 's/^'"$2"':\s*//p'; exit $? }
+ginfo() { glados show "$1" | sed -n 's/^'"$2"':\s*//p' }
