119797f2b43caa4e71f59ad3cd118579aa6e51d3
Ross Light <ross@zombiezen.com>
2013-03-12 22:36:53 -0700

    s/g(cd\|info)/G\1/ in aliases
    This better aligns with "G" being the GLaDOS prefix.  Also keeps consistent with
    Vim commands.

--- misc/ross-aliases.zsh
+++ misc/ross-aliases.zsh
@@ -2,10 +2,10 @@
 
 alias G=glados
 
-gcd() { cd "$(glados path "$1")"; return $? }
-compdef '_values "glados projects" $(glados list 2>/dev/null)' gcd
+Gcd() { cd "$(glados path "$1")"; return $? }
+compdef '_values "glados projects" $(glados list 2>/dev/null)' Gcd
 
-ginfo() { glados show "$1" | sed -n 's/^'"$2"':\s*//p' }
+Ginfo() { glados show "$1" | sed -n 's/^'"$2"':\s*//p' }
 
 alias GPUSH='hg push -R "$GLADOS_PATH"'
 alias GPULL='hg pull -R "$GLADOS_PATH" -u'
