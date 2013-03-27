9a874eef7ea70e084d1dfb2e3f4314d938cfd9b9
Ross Light <ross@zombiezen.com>
2013-03-27 13:27:28 -0700

    Improve search based on token context (closes #31)
    I should probably document the formula on a wiki page, but now search results
    are based on the weighted sum of the token's appearance and all tokens must
    appear in the project.

--- catalog/search/search.go
+++ catalog/search/search.go
@@ -24,7 +24,7 @@ type Result struct {
 
 type textSearch struct {
 	c catalog.Catalog
-	i map[string][]string
+	i map[string][]indexEntry
 }
 
 // NewTextSearch returns a Searcher that performs full text search over the
@@ -34,7 +34,7 @@ type textSearch struct {
 func NewTextSearch(cat catalog.Catalog) (Searcher, error) {
 	ts := &textSearch{
 		c: cat,
-		i: make(map[string][]string),
+		i: make(map[string][]indexEntry),
 	}
 	names, err := cat.List()
 	if err != nil {
@@ -54,24 +54,77 @@ func (ts *textSearch) Search(q string) ([]Result, error) {
 		return nil, nil
 	}
 
-	results := make(map[string]*Result)
-	tokenScale := 1.0 / float32(len(tokens))
-	for _, tok := range tokens {
-		tsi := ts.i[tok]
-		quantum := 1.0 / float32(len(tsi))
-		for _, sn := range tsi {
-			r := results[sn]
-			if r == nil {
-				r = &Result{ShortName: sn}
-				results[sn] = r
+	// Get results for each individual search term
+	type im struct {
+		i int
+		m map[string]*Result
+	}
+	c := make(chan im)
+	for i, tok := range tokens {
+		go func(i int, tok string) {
+			results := make(map[string]*Result)
+			tsi := ts.i[tok]
+			if len(tsi) == 0 {
+				c <- im{i, results}
+				return
 			}
-			r.Relevance += quantum * tokenScale
-		}
+
+			for _, ent := range tsi {
+				sn := ent.shortName
+				r := results[sn]
+				if r == nil {
+					r = &Result{ShortName: sn}
+					results[sn] = r
+				}
+				r.Relevance += ent.kind.Weight()
+			}
+
+			var maxScore float32
+			for _, r := range results {
+				if r.Relevance > maxScore {
+					maxScore = r.Relevance
+				}
+			}
+			for _, r := range results {
+				r.Relevance /= maxScore
+			}
+
+			c <- im{i, results}
+		}(i, tok)
 	}
 
-	resultSlice := make([]Result, 0, len(results))
-	for _, r := range results {
-		resultSlice = append(resultSlice, *r)
+	// Collect results for each term
+	master := make([]map[string]*Result, len(tokens))
+	minIdx := -1
+	for nret := 0; nret < len(tokens); nret++ {
+		ret := <-c
+		master[ret.i] = ret.m
+		if minIdx == -1 || len(ret.m) < len(master[minIdx]) {
+			minIdx = ret.i
+		}
+	}
+	maxResults := len(master[minIdx])
+	if maxResults == 0 {
+		return []Result{}, nil
+	}
+
+	// Filter results and re-normalize relevance
+	resultSlice := make([]Result, 0, maxResults)
+	for sn := range master[minIdx] {
+		result := Result{
+			ShortName: sn,
+		}
+		for _, m := range master {
+			if r := m[sn]; r == nil {
+				result.Relevance = 0
+				break
+			} else {
+				result.Relevance += r.Relevance / float32(len(tokens))
+			}
+		}
+		if result.Relevance > 0 {
+			resultSlice = append(resultSlice, result)
+		}
 	}
 	sort.Sort(byRelevance(resultSlice))
 	return resultSlice, nil
@@ -82,23 +135,53 @@ func (ts *textSearch) build(sn string) error {
 	if err != nil {
 		return err
 	}
-	words := tokenize(p.Name)
-	words = append(words, fold(p.ShortName))
-	words = append(words, tokenize(p.Description)...)
+	ts.index(sn, kindShortName, sn)
+	ts.index(sn, kindName, tokenize(p.Name)...)
+	ts.index(sn, kindDescription, tokenize(p.Description)...)
 	for _, tag := range p.Tags {
-		words = append(words, fold(tag))
+		ts.index(sn, kindTag, tag)
 		if parts := strings.Split(tag, "-"); len(parts) > 1 {
-			for _, part := range parts {
-				if part != "" {
-					words = append(words, fold(part))
-				}
-			}
+			ts.index(sn, kindTagPart, parts...)
 		}
 	}
-	for _, w := range words {
-		ts.i[w] = append(ts.i[w], sn)
-	}
 	return nil
+}
+
+func (ts *textSearch) index(sn string, kind entryKind, words ...string) {
+	for _, w := range words {
+		if w != "" {
+			w = fold(w)
+			ts.i[w] = append(ts.i[w], indexEntry{sn, kind})
+		}
+	}
+}
+
+type indexEntry struct {
+	shortName string
+	kind      entryKind
+}
+
+type entryKind int
+
+// Index entry kinds
+const (
+	kindDescription entryKind = iota
+	kindTagPart
+	kindTag
+	kindName
+	kindShortName
+)
+
+var kindWeights = [...]float32{
+	kindDescription: 0.01,
+	kindTagPart:     0.7,
+	kindTag:         0.8,
+	kindName:        0.9,
+	kindShortName:   0.95,
+}
+
+func (k entryKind) Weight() float32 {
+	return kindWeights[k]
 }
 
 func fold(s string) string {
