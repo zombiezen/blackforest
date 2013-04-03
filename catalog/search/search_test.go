231f160322c90549f719f7f03b0b5d83125739ba
Ross Light <ross@zombiezen.com>
2013-04-03 14:33:39 -0700

    Add benchmarks for Search

--- catalog/search/search_test.go
+++ catalog/search/search_test.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"strconv"
 	"testing"
 
 	"bitbucket.org/zombiezen/glados/catalog"
@@ -200,6 +201,66 @@ func TestTextSearch(t *testing.T) {
 	}
 }
 
+func newTestCatalog() catalog.Catalog {
+	cat := mockCatalog{
+		"go": &catalog.Project{
+			ShortName:   "go",
+			Name:        "Go",
+			Tags:        catalog.TagSet{"compiler", "external", "lang-c", "lang-go", "language"},
+			Description: "Go is an open source programming environment that makes it easy to build simple, reliable, and efficient software.",
+		},
+		"python": &catalog.Project{
+			ShortName:   "python",
+			Name:        "Python",
+			Tags:        catalog.TagSet{"interpreter", "external", "lang-c", "lang-python", "language"},
+			Description: "Python is a general-purpose, high-level programming language whose design philosophy emphasizes code readability.",
+		},
+	}
+	for i := 0; i < 1000; i++ {
+		sn := "PROJECT_" + strconv.Itoa(i)
+		cat[sn] = &catalog.Project{
+			ShortName: sn,
+			Name: sn,
+			Tags: catalog.TagSet{"junk"},
+			Description: "Lorem ipsum",
+		}
+	}
+	return cat
+}
+
+func BenchmarkTextSearchIndex(b *testing.B) {
+	b.StopTimer()
+	cat := newTestCatalog()
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		NewTextSearch(cat)
+	}
+}
+
+func BenchmarkTextSearchAnd(b *testing.B) {
+	b.StopTimer()
+	cat := newTestCatalog()
+	searcher, _ := NewTextSearch(cat)
+	ts := searcher.(*textSearch)
+	q, _ := parseQuery("go go go go go go go")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		ts.search(q)
+	}
+}
+
+func BenchmarkTextSearchOr(b *testing.B) {
+	b.StopTimer()
+	cat := newTestCatalog()
+	searcher, _ := NewTextSearch(cat)
+	ts := searcher.(*textSearch)
+	q, _ := parseQuery("PYTHON OR GO OR PYTHON OR GO OR PYTHON")
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		ts.search(q)
+	}
+}
+
 func TestFold(t *testing.T) {
 	tests := []struct {
 		S      string
@@ -217,6 +278,22 @@ func TestFold(t *testing.T) {
 			t.Errorf("fold(%q) = %q; want %q", test.S, f, test.Folded)
 		}
 	}
+}
+
+func BenchmarkFold(b *testing.B) {
+	const n = 65536
+	b.StopTimer()
+	r := make([]rune, 0, n)
+	for i := 0; i < n; i++ {
+		r = append(r, rune(i))
+	}
+	s := string(r)
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		fold(s)
+	}
+	b.SetBytes(n)
 }
 
 type mockCatalog map[string]*catalog.Project
